b708f405d9420439306e6d470ac053b9ec97321f
auc114514 <1105634705@qq.com>
2026-08-27 16:16:19 +0800

    Add apiCall function for API requests

--- public/js/api.js
+++ public/js/api.js
@@ -1,1 +1,11 @@
-
+function apiCall(endpoint, opts = {}) {
+    const token = localStorage.getItem('dcf_token');
+    return fetch(API + endpoint, {
+        ...opts,
+        headers: {
+            'Content-Type': 'application/json',
+            ...(token ? { 'Authorization': 'Bearer ' + token } : {}),
+            ...opts.headers
+        }
+    }).then(r => r.json()).catch(() => ({ error: '网络错误' }));
+}
